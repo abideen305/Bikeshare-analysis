84ec40afeaa38e5efe61fc1436a531ee6580f312
abideen305 <abideenmuhammed2018@gmail.com>
2020-09-19 17:49:41 +0100

    first commit: resolving around the sections of the project

---
+++ user_types.py
@@ -0,0 +1,16 @@
+#: Display a Breakdown of User Types
+#There are different types of users specified in the "User Type" column. Find how many there are of each type and store the counts in a pandas Series in the user_types variable.
+
+#Hint: What pandas function returns a Series with the counts of each unique value in a column?
+
+import pandas as pd
+
+filename = 'chicago.csv'
+
+# load data file into a dataframe
+df = 
+
+# print value counts for each user type
+user_types = 
+
+print(user_types)
